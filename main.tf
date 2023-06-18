f6168e2c18cc576ba32afbfc87cf8d0477e52bf0
Shravang007 <shravangoudgajula@gmail.com>
2023-06-18 19:13:47 +0530

    terraform

--- main.tf
+++ main.tf
@@ -48,11 +48,6 @@ resource "aws_iam_instance_profile" "instance_profile" {
   role = aws_iam_role.role.name
 }
 
-resource "aws_iam_role_policy_attachment" "policy-attach" {
-  role       = aws_iam_role.role.name
-  policy_arn = aws_iam_policy.policy.arn
-}
-
 #3. Security Group
 
 resource "aws_security_group" "sg" {
@@ -86,11 +81,9 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = [aws_security_group.sg.id]
   iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
 
-  tags = merge({
+  tags = {
     Name = "${var.component}-${var.env}"
-  },
-    var.tags)
-
+  }
 }
 
 #5. DNS Record (Route 53)
