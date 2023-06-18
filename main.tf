ed999850d3512f439dca487448473f94cddd6281
Shravang007 <shravangoudgajula@gmail.com>
2023-06-18 12:11:16 +0530

    terraform

--- main.tf
+++ main.tf
@@ -48,6 +48,8 @@ resource "aws_iam_instance_profile" "instance_profile" {
   role = aws_iam_role.role.name
 }
 
+#Nothing
+
 resource "aws_iam_role_policy_attachment" "policy-attach" {
   role       = aws_iam_role.role.name
   policy_arn = aws_iam_policy.policy.arn
