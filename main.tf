399fa5926eb1ecdf93e94d71ae154755a9a9c493
Shravang007 <shravangoudgajula@gmail.com>
2023-06-18 11:59:56 +0530

    terraform

--- main.tf
+++ main.tf
@@ -29,16 +29,16 @@ resource "aws_iam_role" "role" {
   name = "${var.component}-${var.env}-ec2-role"
 
   assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
+    "Version" : "2012-10-17"
     "Statement" : [
       {
-        "Effect" : "Allow",
+        "Effect" : "Allow"
         "Principal" : {
           "Service" : "ec2.amazonaws.com"
-        },
+        }
         "Action" : "sts:AssumeRole"
         }
-      }
+      },
     ]
   })
 }
