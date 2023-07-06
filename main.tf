6b5393401dffecc39336521521d56427a197c661
Shravang007 <shravangoudgajula@gmail.com>
2023-07-07 00:00:40 +0530

    app module setup

--- main.tf
+++ main.tf
@@ -36,6 +36,23 @@ resource "aws_lb_target_group" "main" {
   vpc_id   = var.vpc_id
   }
 
+resource "aws_lb_listener_rule" "static" {
+  listener_arn = var.listener_arn
+  priority     = var.lb_rule_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.component}-${var.env}.devopspractice73.online"]
+    }
+  }
+}
+
+
 
   resource "aws_launch_template" "main" {
     name = "${var.component}-${var.env}"
