9a3cdcc80e922e8b4c47feac7e19978c95d29f27
Shravang007 <shravangoudgajula@gmail.com>
2023-07-06 20:29:36 +0530

    app module setup

--- userdata.sh
+++ userdata.sh
@@ -3,3 +3,7 @@
 set-hostname -skip-apply ${component}
 labauto ansible
 ansible-pull -i localhost, -U https://github.com/Shravang007/roboshop-ansible main.yml -e role_name=${component} -e env=${env} &>>/opt/ansible.log
+
+
+
+
