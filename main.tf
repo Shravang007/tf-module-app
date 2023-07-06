f9eb8b0d27d39d56f5e14434d6b91cedb4a18126
Shravang007 <shravangoudgajula@gmail.com>
2023-07-06 22:05:56 +0530

    app module setup

--- main.tf
+++ main.tf
@@ -36,12 +36,12 @@ resource "aws_security_group" "main" {
   }
 }
 
-#resource "aws_lb_target_group" "main" {
-#  name                 = "${var.component}-${var.env}-tg"
-#  port                 = var.app_port
-#  protocol             = "HTTP"
-##  deregistration_delay = 30
-#  vpc_id               = var.vpc_id
+resource "aws_lb_target_group" "main" {
+  name     = "${var.component}-${var.env}-tg"
+  port     = var.app_port
+  protocol = "HTTP"
+  #  deregistration_delay = 30
+  vpc_id   = var.vpc_id
 
 #  health_check {
 #    enabled             = true
@@ -52,72 +52,71 @@ resource "aws_security_group" "main" {
 #    timeout             = 4
 #    healthy_threshold   = 2
 #    unhealthy_threshold = 2
-#  }
-
-
-#resource "aws_lb_listener_rule" "static" {
-#  listener_arn = var.listener_arn
-#  priority     = var.lb_rule_priority
-#
-#  action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.main.arn
-#  }
-#
-#  condition {
-#    host_header {
-#      values = ["${var.component}-${var.env}.rdevopsb73.online"]
-#    }
-#  }
-#}
-
-
-resource "aws_launch_template" "main" {
-  name = "${var.component}-${var.env}"
-
-  iam_instance_profile {
-    name = aws_iam_instance_profile.instance_profile.name
-  }
-  image_id               = data.aws_ami.ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.main.id]
-
-  tag_specifications {
-    resource_type = "instance"
-    tags          = merge({ Name = "${var.component}-${var.env}", Monitor = "true" }, var.tags)
   }
 
-  user_data = base64encode(templatefile("${path.module}/userdata.sh",
-    {
-      env       = var.env
-      component = var.component
-    }))
-}
 
-#  block_device_mappings {
-#    device_name = "/dev/sda1"
-#
-#    ebs {
-#      volume_size = 10
-#      encrypted   = "true"
-#      kms_key_id  = var.kms_key_id
-#    }
-#  }
-#}
-#
-resource "aws_autoscaling_group" "main" {
-  desired_capacity    = var.desired_capacity
-  max_size            = var.max_size
-  min_size            = var.min_size
-  vpc_zone_identifier = var.subnets
-#  target_group_arns   = [ aws_lb_target_group.main.arn ]
+  #resource "aws_lb_listener_rule" "static" {
+  #  listener_arn = var.listener_arn
+  #  priority     = var.lb_rule_priority
+  #
+  #  action {
+  #    type             = "forward"
+  #    target_group_arn = aws_lb_target_group.main.arn
+  #  }
+  #
+  #  condition {
+  #    host_header {
+  #      values = ["${var.component}-${var.env}.rdevopsb73.online"]
+  #    }
+  #  }
+  #}
 
 
-  launch_template {
-    id      = aws_launch_template.main.id
-    version = "$Latest"
+  resource "aws_launch_template" "main" {
+    name = "${var.component}-${var.env}"
+
+    iam_instance_profile {
+      name = aws_iam_instance_profile.instance_profile.name
+    }
+    image_id               = data.aws_ami.ami.id
+    instance_type          = var.instance_type
+    vpc_security_group_ids = [aws_security_group.main.id]
+
+    tag_specifications {
+      resource_type = "instance"
+      tags          = merge({ Name = "${var.component}-${var.env}", Monitor = "true" }, var.tags)
+    }
+
+    user_data = base64encode(templatefile("${path.module}/userdata.sh",
+      {
+        env       = var.env
+        component = var.component
+      }))
   }
-}
+
+  #  block_device_mappings {
+  #    device_name = "/dev/sda1"
+  #
+  #    ebs {
+  #      volume_size = 10
+  #      encrypted   = "true"
+  #      kms_key_id  = var.kms_key_id
+  #    }
+  #  }
+  #}
+  #
+  resource "aws_autoscaling_group" "main" {
+    desired_capacity    = var.desired_capacity
+    max_size            = var.max_size
+    min_size            = var.min_size
+    vpc_zone_identifier = var.subnets
+    target_group_arns   = [ aws_lb_target_group.main.arn ]
+
+    launch_template {
+      id      = aws_launch_template.main.id
+      version = "$Latest"
+    }
+  }
 
 
 
